c857e78ba70afb04481a466332545a01e8940064
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2023-02-13 23:44:38 -0500

    init commit

---
+++ app/src/main/java/com/example/myfirsttaskadam/ui/people/PeopleViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.myfirsttaskadam.ui.people
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PeopleViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/myfirsttaskadam/ui/room/RoomFragment.kt
@@ -0,0 +1,42 @@
+package com.example.myfirsttaskadam.ui.room
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myfirsttaskadam.databinding.FragmentRoomBinding
+
+class RoomFragment : Fragment() {
+
+    private var _binding: FragmentRoomBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(RoomViewModel::class.java)
+
+        _binding = FragmentRoomBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
